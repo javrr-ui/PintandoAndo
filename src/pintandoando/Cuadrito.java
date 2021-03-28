cb75740541645ca486ed686e98cd64253e76e732
javrr-ui <67305880+javrr-ui@users.noreply.github.com>
2021-03-27 21:26:13 -0700

    agregada clase cuadrito

---
+++ src/pintandoando/Cuadrito.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pintandoando;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+/**
+ *
+ * @author Javi
+ */
+public class Cuadrito {
+    
+    private int xPos=50;
+    private int yPos=50;
+    private int width=20;
+    private int height=20;
+    
+    
+    Cuadrito(){
+        
+    }
+    
+    
+    public void paintCuadro(Graphics g){
+        
+        g.setColor(Color.red);
+        g.fillRect(xPos, yPos, width, height);
+        g.setColor(Color.black);
+        g.drawRect(xPos,yPos,width,height);
+        
+    }
+
+    public int getX() {
+        return xPos;
+    }
+
+    public void setX(int xPos) {
+        this.xPos = xPos;
+    }
+
+    public int getY() {
+        return yPos;
+    }
+
+    public void setY(int yPos) {
+        this.yPos = yPos;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    
+    
+    
+}
